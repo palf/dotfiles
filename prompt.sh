7311c3148450d2c35310478e2b41a858e0f32ea6
Jack Palfrey <jack.palf@gmail.com>
2015-05-08 22:34:29 +0100

    (prompt) split prompt definition into small functions

--- prompt.sh
+++ prompt.sh
@@ -1,20 +1,40 @@
 #/usr/bin/env bash
 
 parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
-clear="\e[0m"
-intense="\e[1m"
+reset="\e[\033[1;0m\]\]"
+clear="\e[0m\]"
+intense="\e[1m\]"
 
-black="\e[30m"
-red="\e[31m"
-green="\e[32m"
-yellow="\e[33m"
-blue="\e[34m"
-purple="\e[35m"
-cyan="\e[36m"
-white="\e[37m"
+black="\e[30m\]"
+red="\e[31m\]"
+green="\e[32m\]"
+yellow="\e[33m\]"
+blue="\e[34m\]"
+purple="\e[35m\]"
+cyan="\e[36m\]"
+white="\e[37m\]"
 
-export PS1="\n$intense$blue\h: $white\w\n$purple\$(parse_git_branch)$clear$white\]λ "
+user_host="$intense$blue\h"
+current_folder="$intense$red\w"
+git_branch="$intense$purple\$(parse_git_branch)"
+
+wrap() {
+  echo "$1$reset "
+}
+
+lines=()
+lines[0]="$(wrap $user_host)"
+lines[1]="$(wrap $current_folder)"
+lines[2]="$(wrap $git_branch)"
+
+function join { local IFS="$1"; shift; echo "$*"; }
+
+headline="$(join \| ${lines[@]})"
+
+export PS1="
+$headline
+λ$reset "
 
