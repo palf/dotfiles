e3e15ee3afc07141bb2bb5dc67d42ec6632f6a7e
Jack Palfrey <jack.palf@gmail.com>
2015-07-18 22:12:48 +0100

    (prompt) removing loop and fixing colors

--- prompt.sh
+++ prompt.sh
@@ -5,35 +5,29 @@ parse_git_branch() {
 }
 
 clear="\[\e[0m\]"
-intense="\[\e[0m\]"
+intense="\[\e[1m\]"
 
 black="\[\e[30m\]"
 red="\[\e[31m\]"
 green="\[\e[32m\]"
 yellow="\[\e[33m\]"
 blue="\[\e[34m\]"
-purple="\[\e[35m\]"
+magenta="\[\e[35m\]"
 cyan="\[\e[36m\]"
 white="\[\e[37m\]"
 
-user_host="$intense$blue\h"
-current_folder="$intense$red\w"
-git_branch="$intense$purple\$(parse_git_branch)"
+host="$black\h"
+current_folder="$blue\w"
+git_branch="$red\$(parse_git_branch)"
 
-wrap() {
-  echo "$1$clear"
-}
+time="$black\D{%T}"
+history="$red\!"
+symbol="${blue}λ"
 
-lines=()
-lines[0]="$(wrap $user_host)"
-lines[1]="$(wrap $current_folder)"
-lines[2]="$(wrap $git_branch)"
-
-function join { local IFS="$1"; shift; echo "$*"; }
-
-headline="$(join \| ${lines[@]})"
+headline="$host $current_folder $git_branch"
+baseline="$time $history $symbol"
 
 export PS1="$clear
-$headline
-$red\!$purple λ$clear "
+$headline $clear
+$baseline $clear"
 
