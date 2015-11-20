3f93ac38cd8b777978ff47b200c5e5cc2075fd6e
Jack Palfrey <jack.palf@gmail.com>
2015-11-20 14:34:22 +0000

    (fzf) adding show command to browse git history

--- fzf_commands.sh
+++ fzf_commands.sh
@@ -53,3 +53,15 @@ fco() {
     git checkout $(echo "$commit" | sed "s/ .*//")
 }
 
+fshow() {
+	local out sha q
+	while out=$(
+		git log --graph --color=always \
+			--format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+		fzf --ansi --multi --no-sort --reverse --query="$q" --print-query); do
+		q=$(head -1 <<< "$out")
+		while read sha; do
+			git show --color=always $sha | less -R
+		done < <(sed '1d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
+	done
+}
