13974f39022a7be22a375664773c6d6022cb52a9
Jack Palfrey <jack.palf@gmail.com>
2016-01-21 17:54:00 +0000

    (commands) adding some utility functions for repository inspection

--- functions.sh
+++ functions.sh
@@ -1,3 +1,15 @@
 git_stats() {
 	git log --author="$1" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -
 }
+
+git_authors() {
+	git log --format='%aN' | sort -u
+}
+
+gnb() {
+	git fetch origin ; git co -b $1 origin/master ; git push your_remote $1 ; git branch --set-upstream-to=your_remote/$1
+}
+
+grb() {
+	git fetch origin ; git rebase -i origin/master
+}
