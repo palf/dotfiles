c4586ea8b472f0e2ff9a4b322b55011a7aceb14f
Jack Palfrey <jack.palf@gmail.com>
2016-03-12 21:05:55 +0000

    (alias) removing unused aliases

--- aliases.sh
+++ aliases.sh
@@ -9,16 +9,7 @@ alias gl="git log"
 alias gg="git graph"
 
 alias ls="ls -alGh"
-
 alias tree="tree -C"
-alias t2="tree -L 2"
-alias t3="tree -L 3"
-
 alias be="bundle exec"
 alias serve="python -m SimpleHTTPServer"
-
-alias hist="history"
-alias uh="hist | sed -e 's/.*\([0-9]\{2\}:[0-9]\{2\}\) \{1,2\}\(.*\)/\2/' | sort | uniq"
-
-alias count_words="grep -v \"^\s*$\" | sort | uniq -c | sort -bnr"
 
