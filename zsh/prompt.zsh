f6a1af8450343f7f12c1368728df0f438450fce6
Jack Palfrey <jack.palf@gmail.com>
2016-04-13 20:05:27 +0100

    (zsh) rewriting prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,27 +1,37 @@
-function git_prompt_info {
-	local ref=$(=git symbolic-ref HEAD 2> /dev/null)
-	local gitst="$(=git status 2> /dev/null)"
+function git_status {
+  local ref=$(=git symbolic-ref HEAD 2> /dev/null)
+  local gitst="$(=git status 2> /dev/null)"
 
-	if [[ -f .git/MERGE_HEAD ]]; then
-		if [[ ${gitst} =~ "unmerged" ]]; then
-			gitstatus=" %{$fg[red]%}unmerged%{$reset_color%}"
-		else
-			gitstatus=" %{$fg[green]%}merged%{$reset_color%}"
-		fi
-	elif [[ ${gitst} =~ "Changes to be committed" ]]; then
-		gitstatus=" %{$fg[blue]%}!%{$reset_color%}"
-	elif [[ ${gitst} =~ "use \"git add" ]]; then
-		gitstatus=" %{$fg[red]%}!%{$reset_color%}"
-	elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
-		gitstatus=" %{$fg[yellow]%}*%{$reset_color%}"
-	else
-		gitstatus=''
-	fi
+  if [[ -f .git/MERGE_HEAD ]]; then
+    if [[ ${gitst} =~ "unmerged" ]]; then
+      gitstatus="${PR_BOLD_RED}unmerged "
+    else
+      gitstatus="${PR_BOLD_GREEN} merged "
+    fi
+  elif [[ ${gitst} =~ "Changes to be committed" ]]; then
+    gitstatus="${PR_BOLD_GREEN}! "
+  elif [[ ${gitst} =~ "use \"git add" ]]; then
+    gitstatus="${PR_BOLD_RED}! "
+  elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
+    gitstatus="${PR_BOLD_YELLOW}* "
+  else
+    gitstatus=""
+  fi
 
-	if [[ -n $ref ]]; then
-		echo "${PR_BOLD_GREEN}(${ref#refs/heads/})%{$reset_color%}$gitstatus "
-	fi
+  if [[ -n $ref ]]; then
+    echo "$gitstatus%{$reset_color%}"
+  fi
 }
 
-PROMPT='$(git_prompt_info)${PR_BLUE}%(!.#.»)%{$reset_color%} '
-RPROMPT='${PR_BOLD_WHITE}%~%<<%{${reset_color}%}'
+function git_branch {
+  local ref
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+
+  if [[ -n $ref ]]; then
+    echo "${PR_BLUE}(${ref#refs/heads/})%{$reset_color%}"
+  fi
+}
+
+PROMPT='$(git_status)${PR_BOLD_BLUE}%(!.#.»)%{$reset_color%} '
+# RPROMPT='${PR_WHITE}%~%<<%{${reset_color}%}'
+RPROMPT='$(git_branch)'
