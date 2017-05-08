93b558c17cdaaf7244136d08783845edaac284e9
Jack Palfrey <jack.palf@gmail.com>
2017-05-08 23:48:41 +0100

    (DO NOT COMMIT) adding paths

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,3 +1,8 @@
 ANDROID_PATH=/home/palf/tools/android/sdk/platform-tools
+MINICONDA_PATH=/home/palf/miniconda3/bin
 
-export PATH=$ANDROID_SDK:$PATH
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+
+export PATH="$MINICONDA_PATH:$ANDROID_SDK:$PATH"
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
